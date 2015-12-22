078e6032c82b51a760a1e10cc2c89cc8b4332cc3
Brennan Novak <hi@bnvk.me>
2015-12-22 01:24:47 +0100

    added Dropdown support to Navigation bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,4 +26,17 @@ $(document).ready(function() {
     }
   });
 
+  // Dropdowns
+  $(".dropdown-button").click(function() {
+    var $button, $menu;
+    $button = $(this);
+    $menu = $button.siblings(".dropdown-menu");
+    $menu.toggleClass("show-menu");
+    $menu.children("li").click(function() {
+      $menu.removeClass("show-menu");
+      $button.html($(this).html());
+    });
+  });
+
+
 });
